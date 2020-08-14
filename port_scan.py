70de728f4ab255536bcec18d464796b2bc35ade3
Phillip Gilligan <philip.giligan@outlook.com>
2020-08-14 02:10:16 -0500

    Added port/services json file, working out wierd for loop bbug

--- port_scan.py
+++ port_scan.py
@@ -6,38 +6,48 @@ from rich import print as rprint
 from rich.console import Console
 from rich.table import Table
 from rich.progress import track
+import json
 
 ascii_banner = pyfiglet.figlet_format("PORT SCANNER", font='block')
 ascii_author = pyfiglet.figlet_format("By Binary Fallen", font='cybersmall')
 rprint(ascii_banner) 
-rprint(ascii_author) 
+rprint(ascii_author)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(5)
-
-host = input("Please enter the IP you want to scan: ")
-portstart = int(input("Starting Port: "))
-portend = int(input("Ending Port: "))
-portlist = []
-portlist.extend(range(portstart, portend+1))
+port = "80"
+data = json.load(open("port-db.json"))
+for key, value in data.items():
+    if key == port:
+        rprint(key, value) # example usage
 
 
-def portScanner(portlist):
-    table = Table(title=f"Port Scan for Host: {host}")
-    table.add_column("Port Number", justify="left", style="cyan", no_wrap=True)
-    table.add_column("Status", style="magenta")
-    table.add_column("Service", justify="right", style="green")
+# host = input("Please enter the IP you want to scan: ")
+
+# def portScanner():
+#     portstart = int(input("start: "))
+#     portstop = int(input("stop: "))
+#     portlist = []
+#     portlist.extend(range(portstart, portstop))
+#     table = Table(title=f"Port Scan for Host: {host}")
+#     table.add_column("Port Number", justify="center", style="cyan", no_wrap=True)
+#     table.add_column("Status", style="magenta")
+#     table.add_column("Service", justify="left", style="yellow")
     
-    for port in track(range(portend), description="Processing..."):
-        if s.connect_ex((host, port)):
-            table.add_row(f"{port}", "[bold red]Closed[/bold red]", "Unknown")
-        else:
-            table.add_row(f"{port}", "[bold green]Open[/bold green]", "Unknown")
+#     for port in portlist:
+#         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#         s.settimeout(.25)
+#         try:
+#             s.connect((host, port))
+#             s.close()
+#             table.add_row(f"{port}", "[bold green]Open[/bold green]", "{x}")
+            
+#         except (socket.timeout, socket.error):
+#             table.add_row(f"{port}", "[bold red]Closed[/bold red]", "Unknown")
+
     
-    rprint ("Scanning Completed")
-    console = Console()
-    console.print(table)
+#     rprint ("Scanning Completed")
+#     console = Console()
+#     console.print(table)
 
-portScanner(portlist)
+# portScanner()
 
-#for n in track(range(n), description="Processing..."):
+hold = input("Scanning completed, press any key to exit")
