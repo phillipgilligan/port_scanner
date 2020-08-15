8765863feb014731180b2ba278a878c937d06e2e
Phillip Gilligan <philip.giligan@outlook.com>
2020-08-15 01:55:32 -0500

    Fixed error pulling data from json, for now commented out on closed ports for display quality

--- port_scan.py
+++ port_scan.py
@@ -13,41 +13,44 @@ ascii_author = pyfiglet.figlet_format("By Binary Fallen", font='cybersmall')
 rprint(ascii_banner) 
 rprint(ascii_author)
 
-port = "80"
+# port = "80"
 data = json.load(open("port-db.json"))
-for key, value in data.items():
-    if key == port:
-        rprint(key, value) # example usage
+# for key, value in data.items():
+#     if key == port:
+#         rprint(key, value) # example usage
 
 
-# host = input("Please enter the IP you want to scan: ")
+host = input("Please enter the IP you want to scan: ")
 
-# def portScanner():
-#     portstart = int(input("start: "))
-#     portstop = int(input("stop: "))
-#     portlist = []
-#     portlist.extend(range(portstart, portstop))
-#     table = Table(title=f"Port Scan for Host: {host}")
-#     table.add_column("Port Number", justify="center", style="cyan", no_wrap=True)
-#     table.add_column("Status", style="magenta")
-#     table.add_column("Service", justify="left", style="yellow")
+def portScanner():
+    portstart = int(input("start: "))
+    portstop = int(input("stop: "))
+    portlist = []
+    portlist.extend(range(portstart, portstop))
+    table = Table(title=f"Port Scan for Host: {host}")
+    table.add_column("Port Number", justify="center", style="cyan", no_wrap=True)
+    table.add_column("Status", style="magenta")
+    table.add_column("Service", justify="left", style="yellow")
     
-#     for port in portlist:
-#         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         s.settimeout(.25)
-#         try:
-#             s.connect((host, port))
-#             s.close()
-#             table.add_row(f"{port}", "[bold green]Open[/bold green]", "{x}")
+    for port in portlist:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(.05)
+        
+        try:
+            s.connect((host, port))
+            s.close()
+            x = data.get(str(port), "Unknown")
+            table.add_row(f"{port}", "[bold green]Open[/bold green]", f"{x}")
             
-#         except (socket.timeout, socket.error):
-#             table.add_row(f"{port}", "[bold red]Closed[/bold red]", "Unknown")
+        except (socket.timeout, socket.error):
+            pass
+            # table.add_row(f"{port}", "[bold red]Closed[/bold red]", "Unknown")
 
     
-#     rprint ("Scanning Completed")
-#     console = Console()
-#     console.print(table)
+    rprint ("Scanning Completed")
+    console = Console()
+    console.print(table)
 
-# portScanner()
+portScanner()
 
 hold = input("Scanning completed, press any key to exit")
